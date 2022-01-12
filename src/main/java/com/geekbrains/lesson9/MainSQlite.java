83cc88062fa9194b013c951295dff558d3f76577
Mirpribili <mirpribili@ya.ru>
2022-01-12 17:25:32 +0300

    complete Theory

--- src/main/java/com/geekbrains/lesson9/MainSQlite.java
+++ src/main/java/com/geekbrains/lesson9/MainSQlite.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class MainSQlite {
     private static Connection connection;
     private static Statement statement;
+    private static PreparedStatement preparedStatement; // для защиты от добавления sql команд в gui пользователем
 
     public static void main(String[] args) {
         try {
@@ -27,12 +28,90 @@ public class MainSQlite {
                 // clearTableExample();
                 // dropTableExample();
 
+
+            //transactionAndPreparedStatementExample();
+
+
+            //batchEx();
+
+            // rollbackEx();
+
+            clearTableExample();
+//  до этого момента мы работали с интерфейсами и совсем не создавали объектов(
+// а тогда вопрос а где реализация? Если мы посмотрим
+                // Connection
+                // Statement
+                // PreparedStatement
+            // то все это интерфейсы sqlite-jdbc.... а где реализация?
+                    // в JDBC  драйвере.
+// Плюсы?
+                // Не работая с реализацией мы спокойно можем менять БД SQLite  на БД PostgeSQL  и тд.
+
+
         }catch (SQLException e){
             e.printStackTrace();
         }finally {
             disconnect();
         }
 
+    }
+
+    private static void rollbackEx() throws SQLException {
+        clearTableExample();
+        statement.executeUpdate("INSERT INTO students (name, score) VALUES ('Bob1', 80);");
+        Savepoint savepoint1 = connection.setSavepoint(); //  запомнили состояние таблицы
+        statement.executeUpdate("INSERT INTO students (name, score) VALUES ('Bob2', 181);");
+        connection.rollback(savepoint1);
+        statement.executeUpdate("INSERT INTO students (name, score) VALUES ('Bob3', 80);");
+// Вопрос сколько студентов будет в таблице?
+        // всего 1 и 1й. тк включен автокомит а почему еще?
+    }
+
+    private static void batchEx() throws SQLException {
+        long time = System.currentTimeMillis();
+        clearTableExample();
+
+        // for speed
+        connection.setAutoCommit(false);
+
+        for (int i = 0; i < 100; i++ ){
+            preparedStatement.setString(1, "Bob" + (i + 1));
+            //preparedStatement.setInt(2, 50);
+            preparedStatement.setObject(2, 50); // <------------!!!
+            //preparedStatement.executeUpdate(); // возвращает int кол-ва затронутых строк в бд.
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch(); // вернет массив где, будет отображено кол-во строк для каждого запроса.
+
+        // for speed
+        connection.commit();  // connection.setAutoCommit(true);
+
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+    private static void transactionAndPreparedStatementExample() throws SQLException {
+        long time = System.currentTimeMillis();
+        clearTableExample();
+
+        // for speed
+        connection.setAutoCommit(false);
+
+        for (int i = 0; i < 100; i++ ){
+            preparedStatement.setString(1, "Bob" + (i + 1));
+            //preparedStatement.setInt(2, 50);
+            preparedStatement.setObject(2, 50); // <------------!!!
+            preparedStatement.executeUpdate(); // возвращает int кол-ва затронутых строк в бд.
+        }
+
+        // for speed
+        connection.commit();  // connection.setAutoCommit(true);
+
+        System.out.println(System.currentTimeMillis() - time); // OMG 11.2s for 100 queries!!
+        // after improvement for speed  100 queries == 0.3s !!
+
+        // Почему такая разница в скорости? тк по умолч. каждый запрос выполнялся как самостоятельная транзакция
+        // Плюсы - скорость
+        // Минусы - при ошибке в цикле мы потеряем все запросы не записав в базу
     }
 
     private static void dropTableExample() throws SQLException {
@@ -70,6 +149,9 @@ public class MainSQlite {
             Class.forName("org.sqlite.JDBC"); // устаревшая строка загружающая класс в память
             connection = DriverManager.getConnection("jdbc:sqlite:main.db");// тут порт localhost, login, pass
             statement = connection.createStatement();
+
+            //  плюс пре компиляция см ниже. Защита от gui инъекций
+            preparedStatement = connection.prepareStatement("INSERT INTO students (name, score) VALUES (?, ?);");
         }catch (ClassNotFoundException | SQLException e){
             throw new SQLException("Unable to connect");
         }
